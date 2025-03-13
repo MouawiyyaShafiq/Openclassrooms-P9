50a16e2cb2c62554fc75fc4c88f69c0b3e6e2f18
MouawiyyaShafiq <mouawiyya.sh@hotmail.com>
2025-03-13 14:33:05 +0100

    Correction bug filter

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,25 +13,25 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
+    (!type ? data?.events: data?.events) || []
+  ).filter((event) => event.type === type || !type 
+
+  ).filter((_, index) => 
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  )
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
+
   return (
     <>
       {error && <div>An error occured</div>}
